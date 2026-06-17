a4addfae2a2326e27490febecb3c74420057c015
Yakuba001 <2767yakuba@gmail.com>
2026-06-17 16:00:07 +0300

    change gimini adress

--- src/main/java/com/orgtgbot/service/services/gemini/GeminiParserService.java
+++ src/main/java/com/orgtgbot/service/services/gemini/GeminiParserService.java
@@ -21,7 +21,7 @@ public class GeminiParserService {
     private final ObjectMapper objectMapper;
 
     private static final String GEMINI_API_URL =
-            "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent";
+            "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent";
 
     public GeminiParserService(GeminiProperties geminiProperties, RestClient.Builder restClient, ObjectMapper objectMapper) {
         this.geminiProperties = geminiProperties;
