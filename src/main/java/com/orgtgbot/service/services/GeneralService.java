adc29df9b8a264ebaca61409868ebdc6ca4d2bf1
Yakuba001 <2767yakuba@gmail.com>
2026-07-03 11:41:40 +0300

    Covered generalServiceEntry custom exception

--- src/main/java/com/orgtgbot/service/services/GeneralService.java
+++ src/main/java/com/orgtgbot/service/services/GeneralService.java
@@ -3,6 +3,7 @@ package com.orgtgbot.service.services;
 import com.orgtgbot.dto.GeneralUpdateDto;
 import com.orgtgbot.entity.GeneralEntry;
 import com.orgtgbot.entity.user.UserWorkspace;
+import com.orgtgbot.exception.exceptions.service.WorkspaceNotFoundException;
 import com.orgtgbot.mapper.GeneralMapper;
 import com.orgtgbot.repository.UserWorkspaceRepository;
 import lombok.RequiredArgsConstructor;
@@ -25,7 +26,9 @@ public class GeneralService {
     @Transactional(readOnly = true)
     public GeneralEntry getSingleEntry(Long chatId) {
        UserWorkspace workspace = userWorkspaceRepository.findByUser_TelegramChatId(chatId)
-                .orElseThrow(() -> new IllegalStateException("Workspace not found for chat: " + chatId));
+                .orElseThrow(() -> new WorkspaceNotFoundException(
+                        chatId,
+                        "Workspace not found in GeneralService -> getSingleEntry"));
         return workspace.getGeneralEntry();
     }
 }
