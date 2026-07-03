11bfc2d7c0cc4adbcbafb2a6dc4ce8610a8b4226
Yakuba001 <2767yakuba@gmail.com>
2026-07-03 15:50:18 +0300

    fix smoke test

--- src/test/java/com/orgtgbot/OrgTgBotApplicationTests.java
+++ src/test/java/com/orgtgbot/OrgTgBotApplicationTests.java
@@ -1,9 +1,12 @@
 package com.orgtgbot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.telegram.telegrambots.longpolling.starter.TelegramBotStarterConfiguration;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {TelegramBotStarterConfiguration.class})
 class OrgTgBotApplicationTests {
 
     @Test
